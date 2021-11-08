458ac9abe7f6a2bc114d2570e9bd1258a9b4e280
Raj Mishra <mrrajmishra573@gmail.com>
2021-11-08 18:33:51 +0530

    [08112021]solving if else exercise

--- if-else.rb
+++ if-else.rb
@@ -1,1 +1,42 @@
 #if-else Exercise
+# a = 20;
+# b = 20;
+#  if a == b
+#  	puts "Both are equal"
+#  else
+#  	puts "Both are unequal" 
+# end
+
+
+# Odd or Even 
+# puts "Enter the  no"
+
+# number = gets.to_i
+
+# if number%2==0
+# 	puts "Number is even"
+
+# else 
+# 	puts "Number is odd"
+# end
+
+# Leap year 
+# puts "Enter the year:";
+# year = gets.to_i;
+# if(year % 100!=0 || year % 400==0 )
+# 	puts "This year is a leap year";
+# else
+# 	puts "This year is not a leap year"
+# end
+		
+#Weather
+puts "Enter your surrounding Temperature:"
+    temp = gets.to_i
+    if (temp < 0)
+    	puts "Freezing weather"
+    elsif (temp >= 0  || temp <10)
+    	puts "Very cold weather"
+    elsif(temp >=10 || temp < 20)
+    	puts "Cold Weather"
+    elsif(temp >=20 || temp <30)
+    elsif(temp>=30 || temp <40)
