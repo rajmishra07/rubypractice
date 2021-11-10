d22c34435fe1ec881c654dddf21e2727a593838b
Raj Mishra <mrrajmishra573@gmail.com>
2021-11-10 18:23:57 +0530

    10112021] practicing loop

--- loop.rb
+++ loop.rb
@@ -1,11 +1,27 @@
 #loop
+
+#while loop
 # i=0;
 # while(i<=5)
 # 	puts i
 # 	i=i+1
 # end
-a=0;
-until (a>=5)
-	puts a;
-	a = a+1;
-end
+
+#Until loop
+
+# a=5;
+# until (a<=0)
+# 	puts a;
+# 	a =a-1;
+# end
+
+
+#for loop
+# i=0;
+# no=20;
+# for i in(0..no)
+# 	if(i%2==0)
+# 		puts i;
+# 	end
+# end
+
