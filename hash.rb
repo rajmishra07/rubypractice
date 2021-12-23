804e7725e248721fb8a3e8ac8f73b1eec8fc197e
Raj Mishra <mrrajmishra573@gmail.com>
2021-12-23 18:53:17 +0530

    [23122021] practicing hash

---
+++ hash.rb
@@ -0,0 +1,29 @@
+
+# Hash
+
+# my_info = {
+#     "first_name" => 'raj mishra',
+#     "gender" => 'male',
+#     "grade" => 'A'    
+# }   
+# puts my_info['grade']
+
+# my_personal_info = {
+#     first_name: 'Raj',
+#     last_name: 'Mishra',
+#     batch: 'one',
+#     email: 'rajmishra@123gmail.com',
+#     phone_no: '1234556676'
+
+# }
+# puts my_personal_info[:email]
+
+# convert_num_into_words = {
+#     "100" =>'Hundred'
+# }
+# puts convert_num_into_words['100']
+
+time1 = Time.new
+puts "Current Time : " + time1.inspect
+
+
