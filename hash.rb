c37b6288d41931c34554ba1b88aad2601ce32a7f
Raj Mishra <mrrajmishra573@gmail.com>
2021-12-30 19:06:37 +0530

    [30122021] ranges

--- hash.rb
+++ hash.rb
@@ -24,18 +24,61 @@
 # puts convert_num_into_words['100']
 
 
+#Date and time
+
+# time1 = Time.new
+# puts "Current Time : " + time1.inspect
+# puts time1.year
+# puts time1.wday
+# puts time1.yday
+# puts time1.month
+# puts time1.day
+# puts time1.min
+# puts time1.sec
+# puts time1.zone
+# puts time1.hour
+
+#Ranges
+
+#printing A to Z .
+
+# b = ('a'..'z').to_a
+# puts b
 
 
-time1 = Time.new
-puts "Current Time : " + time1.inspect
-puts time1.year
-puts time1.wday
-puts time1.yday
-puts time1.month
-puts time1.day
-puts time1.min
-puts time1.sec
-puts time1.zone
-puts time1.hour
+# range2 = ('raja'..'rajz').to_a
+# puts range2
+
+# def selection_of_child(a)
+# $age = a
+# case $age
+# when 0 .. 2
+#    puts "baby"
+# when 3 .. 6
+#    puts "little child"
+# when 7 .. 12
+#    puts "child"
+# when 13 .. 18
+#    puts "youth"
+# else
+#    puts "adult"
+# end
+# end
+# puts "Enter the age of child:"
+# child = gets.to_i   
+# selection_of_child(child)
+
+
+class Raj
+def add(a,b)
+puts a+b
+end
+end
+obj = Raj.new
+obj.add(10,20)
+
+
+
+
 
 
